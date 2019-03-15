b06a8fe9b2683babf494cadac733d749a4588d9c
j2FunOnly <j2funonly@ya.ru>
2019-03-15 10:32:08 +0300

    Fix typo in vcr re record interval config

--- spec/support/vcr_config.rb
+++ spec/support/vcr_config.rb
@@ -7,6 +7,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr/cassettes'
   c.default_cassette_options = {
     record: :new_episodes,
-    re_record_interval: 60 * 60 * 60 * 24 # 24 hours interval in seconds
+    re_record_interval: 60 * 60 * 24 # 24 hours interval in seconds
   }
 end
